e7237029779f7690302e12a23cebcbb09b32c320
mberninger <monika-berninger@gmx.de>
2016-04-16 14:22:24 +0200

    some adjustments

--- getFilteredDataPut.m
+++ getFilteredDataPut.m
@@ -1,52 +1,58 @@
 function [ filteredDataPut ] = getFilteredDataPut( data )
-% This function filters the data and outputs the filtered data of the call
+% This function filters the data and outputs the filtered data of the put
 % options
 
-%% Tabelle aufteilen in Call und Put
+%% extract put options from data
 dataPut = data(data.IsCall == 0, :);
 
-%% Impliziete Volatilitäten ermitteln/laden
+%% evaluate/load implied volatilities
 load impliedVolaPut
 
 % implVolPut = blsimpv(dataPut.DAX,dataPut.Strike,dataPut.EONIAmatched,dataPut.TimeToMaturity,dataPut.OptionPrice,[],[],[],{'Put'});
 
-%% Zusammenfassung der Daten und der impliziten Volatilitäten zu einer Tabelle
-varName = {'Date'; 'OptionPrice'; 'Bid'; 'Ask'; 'Volume'; 'OpenInterest'; 'Strike'; 'Expiry'; 'DAX'; 'EONIAmatched'; 'TimeToMaturity'; 'IsCall';'implVol'};
-dataPut = table( dataPut.Date, dataPut.OptionPrice, dataPut.Bid, dataPut.Ask, dataPut.Volume, dataPut.OpenInterest, dataPut.Strike, dataPut.Expiry, dataPut.DAX, dataPut.EONIAmatched, dataPut.TimeToMaturity, dataPut.IsCall, implVolPut, 'VariableNames', varName);
+%% integrate implied volatilities into dataCall table
+dataPut.implVol = implVolPut;
 
-%% Zunächst werden die Daten gefiltert
 
-%% 1. Schritt: Ober- und Untergrenze von Optionspreisen überprüfen
-% Obergrenze Put: P < K (Strikepreis) * exp(-r (Zinssatz)* T (TimeToMaturity))
-dataPutObergrenze = dataPut(dataPut.OptionPrice > (dataPut.Strike .* exp(-dataPut.EONIAmatched.*dataPut.TimeToMaturity)), : );
-% Untergrenze Put: max(K*e^(-rT)-S_t,0) < P
-dataPutUntergrenze = dataPut(dataPut.OptionPrice < max(((dataPut.Strike .* exp(-dataPut.EONIAmatched .* dataPut.TimeToMaturity)) - dataPut.DAX),0), : );
+%% filtering of the data
 
-% Entfernen der Wertober- und Wertuntergrenzenfehler aus Datensatz
-dataPutFiltered = dataPut(dataPut.OptionPrice > max(((dataPut.Strike .* exp(-dataPut.EONIAmatched .* dataPut.TimeToMaturity)) - dataPut.DAX),0), : );
+%% 1. step: check upper and lower bounds of option prices to avoid arbitrage
+% upper bound put: p < k (strike price) * exp(-r (interest rate)* t (time to maturity))
+discountFactorPut = exp(-dataPut.EONIAmatched .* dataPut.TimeToMaturity);
+putUpperBounds = dataPut.Strike .* discountFactorPut;
+% lower bound put: max(k*exp(-rt)-s_t,0) < p
+putLowerBounds = max(((dataPut.Strike .* discountFactorPut) - dataPut.DAX),0);
 
-%% 2. Schritt: negative Zeitwerte ermitteln
-dataPutFiltered2 = dataPutFiltered(dataPutFiltered.OptionPrice - max((dataPutFiltered.Strike - dataPutFiltered.DAX), 0) > 0, : );
+% store valid observations
+validArbitrageIndsPut = putLowerBounds < dataPut.OptionPrice & ...
+    dataPut.OptionPrice < putUpperBounds;
 
-%% 3. Schritt: 
-% nicht weniger als 20 Tage und nicht mehr als 510 Tage bis zur Maturity
-dataPutFiltered3 = dataPutFiltered2((dataPutFiltered2.TimeToMaturity .* 255) <= 510 , : );
-dataPutFiltered3 = dataPutFiltered3((dataPutFiltered3.TimeToMaturity .* 255) >= 20 , : );
+%% 2. step: check for negative time values and remove them (the time value is closer to zero, the closer the option is at the maturity date, but it is never negative)
+intrinsicValPut = max((dataPut.Strike - dataPut.DAX),0);
+timeValPut = dataPut.OptionPrice - intrinsicValPut;
+validTimeValIndsPut = timeValPut > 0;
 
-% Moneyness nicht unter 0.8 und nicht über 1.2 
-dataPutFiltered4 = dataPutFiltered3((dataPutFiltered3.Strike ./ dataPutFiltered3.DAX) <= 1.2, : );
-dataPutFiltered4 = dataPutFiltered4((dataPutFiltered4.Strike ./ dataPutFiltered4.DAX) >= 0.8, : );
+%% 3. step: check time to maturity, it should be between 20 and 510 days
+maturityInDaysPut = dataPut.TimeToMaturity .* 255;
+validMaturityIndsPut = 20 <= maturityInDaysPut & maturityInDaysPut <= 510;
 
-% Optionspreis nicht unter 5
-dataPutFiltered5 = dataPutFiltered4(dataPutFiltered4.OptionPrice >= 5, : );
+%% 4. step: evaluate and check moneyness, it should be between 0.8 and 1.2
+mnyNessPut = dataPut.Strike ./ dataPut.DAX;
+validMnyNessIndsPut = 0.8 <= mnyNessPut & mnyNessPut <= 1.2;
 
-%% 4. Schritt: Implied Vola nicht unter 5 und nicht über 50 Prozent
-dataPutFiltered6 = dataPutFiltered5(dataPutFiltered5.implVol >= 0.05, : );
-dataPutFiltered6 = dataPutFiltered6(dataPutFiltered6.implVol <= 0.5, : );
+%% 5. step: check option price, it should be bigger than 5 
+validPriceIndsPut = dataPut.OptionPrice >= 5;
 
-%% Zuletzt wird die Tabelle um die Moneyness erweitert
-varName14 = {'Date'; 'OptionPrice'; 'Bid'; 'Ask'; 'Volume'; 'OpenInterest'; 'Strike'; 'Expiry'; 'DAX'; 'EONIAmatched'; 'TimeToMaturity'; 'IsCall'; 'implVol'; 'Moneyness'};
-filteredDataPut = table(dataPutFiltered6.Date, dataPutFiltered6.OptionPrice, dataPutFiltered6.Bid, dataPutFiltered6.Ask, dataPutFiltered6.Volume, dataPutFiltered6.OpenInterest, dataPutFiltered6.Strike, dataPutFiltered6.Expiry, dataPutFiltered6.DAX, dataPutFiltered6.EONIAmatched, dataPutFiltered6.TimeToMaturity, dataPutFiltered6.IsCall, dataPutFiltered6.implVol, dataPutFiltered6.Strike ./ dataPutFiltered6.DAX, 'VariableNames', varName14);
+%% 6. step: check implied volatilities, it should be between 5 and 50 percent
+validImplVolaIndsPut = 0.05 <= dataPut.implVol & dataPut.implVol <= 0.5;
 
+%% integrate moneyness into dataPut table
+dataPut.Moneyness = mnyNessPut;
+
+%% apply all filters
+validIndsPut = validArbitrageIndsPut & validTimeValIndsPut & validMaturityIndsPut & ...
+    validMnyNessIndsPut & validPriceIndsPut & validImplVolaIndsPut;
+
+filteredDataPut = dataPut(validIndsPut, :);
 
 end
