5214845aecd436408cea58a84b0291ba838df652
mberninger <monika-berninger@gmx.de>
2016-04-16 13:25:59 +0200

    some adjustments

--- getFilteredDataCall.m
+++ getFilteredDataCall.m
@@ -2,24 +2,24 @@ function [ filteredDataCall ] = getFilteredDataCall( data )
 % This function filters the data and outputs the filtered data of the call
 % options
 
-%% Tabelle aufteilen in Call Optionen
+%% extract call options from data
 dataCall = data(data.IsCall == 1, :);
 
-%% Impliziete Volatilit�ten ermitteln/laden
+%% evaluate/load implied volatilities
 load impliedVolaCall
 
 % implVolCall = blsimpv(dataCall.DAX,dataCall.Strike,dataCall.EONIAmatched,dataCall.TimeToMaturity,dataCall.OptionPrice,[],[],[],{'Call'});
 
-%% Zusammenfassung der Daten und der impliziten Volatilit�ten zu einer Tabelle
+%% integrate implied volatilities into dataCall table
 
 dataCall.implVol = implVolCall;
 
-%% Zun�chst werden die Daten gefiltert
+%% filtering of the data
 
-%% 1. Schritt: Ober- und Untergrenze von Optionspreisen �berpr�fen, um Arbitragem�glichkeiten zu vermeiden
-% Obergrenze Call: P (Optionspreis) < S_t (Wert des Underlyings)
+%% 1. step: check upper and lower bounds of option prices to avoid arbitrage
+% upper bound call: p (option price) < s_t (underlying)
 callUpperBounds = dataCall.DAX;
-% Untergrenze Call: max(S_t-K*e^(-rT),0) < P
+% lower bound call: max(s_t-k*exp(-rt),0) < p
 discountFactor = exp(-dataCall.EONIAmatched .* dataCall.TimeToMaturity);
 callLowerBounds = max((dataCall.DAX - (dataCall.Strike .* discountFactor)), 0);
 
@@ -27,32 +27,31 @@ callLowerBounds = max((dataCall.DAX - (dataCall.Strike .* discountFactor)), 0);
 dataCallObergrenze = dataCall(dataCall.OptionPrice > callUpperBounds, :);
 dataCallUntergrenze = dataCall(dataCall.OptionPrice < callLowerBounds, :);
 
-% Entfernen der Wertober- und Wertuntergrenzenfehler aus Datensatz
+% store valid observations
 validArbitrageInds = callLowerBounds < dataCall.OptionPrice & ...
     dataCall.OptionPrice < callUpperBounds ;
 
-%% 2. Schritt: negative Zeitwerte ermitteln und aus dem Datensatz entfernen (je n�her Option am Verfallstermin, desto n�her an Null, aber niemals negativ)
+%% 2. step: check for negative time values and remove them (the time value is closer to zero, the closer the option is at the maturity date, but it is never negative)
 
 intrinsicVal = max((dataCall.DAX - dataCall.Strike), 0);
 timeVal = dataCall.OptionPrice - intrinsicVal;
 validTimeValInds = timeVal > 0;
 
-%% 3. Schritt: weitere Filter:
-% nicht weniger als 20 Tage und nicht mehr als 510 Tage bis zur Maturity
+%% 3. step: check time to maturity, it should be between 20 and 510 days
 maturityInDays = dataCall.TimeToMaturity .* 255;
 validMaturityInds = 20 <= maturityInDays & maturityInDays <= 510;
 
-% Moneyness nicht unter 0.8 und nicht �ber 1.2 
+%% 4. step:  evaluate and check moneyness, it should be between 0.8 and 1.2 
 mnyNess = dataCall.Strike ./ dataCall.DAX;
 validMnyNessInds = 0.8 <= mnyNess & mnyNess <= 1.2;
 
-% Optionspreis nicht unter 5
+%% 5. step: check option price, it should be bigger than 5 
 validPriceInds = dataCall.OptionPrice >= 5;
 
-%% 4. Schritt: Implied Vola nicht unter 5 und nicht �ber 50 Prozent
+%% 6. step: check implied volatilities, it should be between 5 and 50 percent
 validImplVolaInds = 0.05 <= dataCall.implVol & dataCall.implVol <= 0.5;
 
-%% Zuletzt wird die Tabelle um die Moneyness erweitert
+%% integrate moneyness into dataCall table
 dataCall.Moneyness = mnyNess;
 
 % apply all filters
