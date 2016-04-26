c63996b9e8c0cc7c3552c553d6822aaeb217f54a
cgroll <groll.christian.edu@gmail.com>
2016-04-26 11:27:29 +0200

    removed for loop in surface plot

--- plotSurface.m
+++ plotSurface.m
@@ -1,22 +1,44 @@
 function [  ] = plotSurface( k, coeff, chosenModel, filteredData, dayChanges )
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-[M,T] = meshgrid(0.8:0.02:1.2,20/225:0.1:510/225);
-Z = zeros(22,21);
-for i = 1:22
-    for j = 1:21
-        thisObs = [M(i,j), M(i,j).^2, T(i,j), T(i,j).^2, M(i,j).*T(i,j)];
-        model = thisObs(chosenModel);
-        modelEquation = [1, model];
-        Z(i,j) = modelEquation*coeff(k,:).';
-    end
-end
+%PLOTSURFACE plot estimated vola surface together with observed prices
+
+%%
+% set up grid for maturity and moneyness
+moneyGrid = 0.8:0.02:1.2;
+maturityGrid = 20/225:0.1:510/225;
+
+[M,T] = meshgrid(moneyGrid, maturityGrid);
+
+% Not required anymore: preallocate surface values
+% Note: never hardcode values; you had zeros(22,21)
+% Z = zeros(size(M));
+
+% vectorize grid values
+mVals = M(:);
+tVals = T(:);
+
+% get all possible explanatory variables
+allExplanVars = [mVals, mVals.^2, tVals, tVals.^2, mVals.*tVals];
+
+% get required explanatory variables only
+thisModelExplanVars = allExplanVars(:, chosenModel);
+modelEquation = [ones(size(mVals, 1), 1) thisModelExplanVars];
+
+% get implied vola values predicted by model
+zVals = modelEquation*coeff(k, :)';
+
+% reshape vector of values to matrix
+Z = reshape(zVals', size(M));
+
+% plot surface with transparancy
+alphaVal = 0.5;
 figure
-surface(M,T,Z)
+surface(M, T, Z, 'FaceAlpha', alphaVal)
 view(3)
 
 hold on;
-scatter3(filteredData.Moneyness(dayChanges(k):dayChanges(k+1)-1),filteredData.TimeToMaturity(dayChanges(k):dayChanges(k+1)-1),filteredData.implVol(dayChanges(k):dayChanges(k+1)-1));
+obsRange = dayChanges(k):dayChanges(k+1)-1;
+scatter3(filteredData.Moneyness(obsRange), filteredData.TimeToMaturity(obsRange), ...
+    filteredData.implVol(obsRange), 'filled');
 xlabel('Moneyness');
 ylabel('Time to Maturity');
 zlabel('implied Volatility');
