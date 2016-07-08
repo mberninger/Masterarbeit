e63c2b7be7a4c885c9ec795e612d0e7b5e64b89a
mberninger <monika-berninger@gmx.de>
2016-07-08 22:29:05 +0200

    Kalman Filter

--- Kalman_Filter.m
+++ Kalman_Filter.m
@@ -1,85 +1,51 @@
-%% Hier möchte ich den Kalman-Filter anwenden, um die Volatilitätsflächen zu bestimmen.
-%TestTestTest
-%% Find vAll, the difference between estimated vola and implied vola for every datapoint:
-laenge = 0;
-s = zeros(1907,1);
-vola = zeros(432810,1);
-for i = 1:1906
-    s(i) = dataPerDay(i+1)-dataPerDay(i);
-    for j = 1+laenge:s+laenge
-        M = filteredDataCall.Moneyness(j);
-        T = filteredDataCall.TimeToMaturity(j);
-        vola(j) = coeff(i,1) + coeff(i,2).*M + coeff(i,3).*M.^2 + coeff(i,4).*T + coeff(i,5).*T.*M;
-    end
-    laenge = laenge + s(i);
+function [mu,eps] = Kalman_Filter(coeff, uniqueDates, filteredDataCall, vola)
+%KALMAN_FILTER evaluates the coefficients for the linear model to evaluate
+%the volatility surface. The state equation of the state space model
+%is a VAR(1) model, the observation equation is a linear regression model
+%to evaluate the implied volatility
+    % The output of this function are the coefficients mu and the variance
+    % eps of the last day
+
+%First the parameters of the VAR(1) model are estimated
+Spec = vgxset('n',6,'nAR',1, 'Constant',true);
+EstSpec1 = vgxvarx(Spec,coeff);
+estParam1 = evalCoeffVar(coeff,EstSpec1.a,EstSpec1.AR);
+
+%Then the residuals of the two equtaions of the state space model are
+%evaluated.
+residualCoeff = coeff - estParam1;
+residualVola = filteredDataCall.implVol - vola;
+
+
+%In order to use the Kalman filter, the initial values for the mean and
+%variance of the coefficients are needed:
+mu = zeros(size(coeff,2),size(coeff,1));
+eps = cov(coeff(1,:))*eye(6);
+
+%omega, the covariance of the residual of the state equation, and A, the
+%parameter matrix of the observation equation, are constant:
+omega = cov(residualCoeff); 
+A = EstSpec1.AR{1,1};
+
+for ii = 2:size(coeff,1)
+    %time update:
+    mu(:,ii) = A * mu(:,ii-1) + EstSpec1.a;
+    eps = A * eps * A' + omega;
+    %measurement update:
+    thisDate = uniqueDates(ii);
+    [thisObs,thisObsSize] = getObs(thisDate,filteredDataCall);
+    H = [ones(thisObsSize,1), thisObs.Moneyness, thisObs.Moneyness.^2, thisObs.TimeToMaturity, thisObs.TimeToMaturity.^2, thisObs.Moneyness .* thisObs.TimeToMaturity];
+    v = thisObs.implVol - H*mu(:,ii);
+    R = cov(residualVola)*eye(thisObsSize);
+    tau = H*eps*H' + R;    
+    %Kalman gain:
+    K = eps * H'*tau^-1;
+    
+    mu(:,ii) = mu(:,ii) + K*v;
+    eps = eps - K*tau*K';
 end
 
-vAll = filteredDataCall.implVol(1:432810) - vola;
+mu = mu';
 
-%% Find v, the difference between implied volatility and modelled vola, for every day
-iVol = zeros(1907,1);
-volaModel = zeros(1907,1);
-for i = 1:1907
-    iVol(i,1) = sum(filteredDataCall.implVol(dataPerDay(i):dataPerDay(i+1)-1))./(dataPerDay(i+1)-dataPerDay(i));
-    M = sum(filteredDataCall.Moneyness(dataPerDay(i):dataPerDay(i+1)-1))./(dataPerDay(i+1)-dataPerDay(i));
-    T = sum(filteredDataCall.TimeToMaturity(dataPerDay(i):dataPerDay(i+1)-1))./(dataPerDay(i+1)-dataPerDay(i));
-    volaModel(i,1) = coeff(i,1) + coeff(i,2).*M + coeff(i,3).*M.^2 + coeff(i,4).*T + coeff(i,5).*T.*M;
 end
-clear i;
-
-v = iVol - volaModel;
-
-
-%% Find w, the difference between the actual coefficient and the modelled coefficient
-modelledCoeff = zeros(1907,5);
-modelledCoeff(1,1) = coeff(1,1);
-w = zeros(1907,5);
-    modelledCoeff(1,1) = coeff(1,1);
-    for i = 2:1907
-        modelledCoeff(i,1) = ARCoeff1.Constant + cell2mat(ARCoeff1.AR) .* modelledCoeff(i-1,1);
-    end
-    
-    w(:,1) = coeff(:,1) - modelledCoeff(:,1);
-%
-modelledCoeff(1,1) = coeff(1,2);
-    for i = 2:1907
-        modelledCoeff(i,1) = ARCoeff2.Constant + cell2mat(ARCoeff2.AR) .* modelledCoeff(i-1,1);
-    end
-    
-    w(:,2) = coeff(:,2) - modelledCoeff(:,1);
-%
-modelledCoeff(1,1) = coeff(1,3);
-    for i = 2:1907
-        modelledCoeff(i,1) = ARCoeff3.Constant + cell2mat(ARCoeff3.AR) .* modelledCoeff(i-1,1);
-    end
-    w(:,3) = coeff(:,3) - modelledCoeff(:,1);
-%
-modelledCoeff(1,1) = coeff(1,4);
-    for i = 2:1907
-        modelledCoeff(i,1) = ARCoeff4.Constant + cell2mat(ARCoeff4.AR) .* modelledCoeff(i-1,1);
-    end
-    w(:,4) = coeff(:,4) - modelledCoeff(:,1);
-%        
-modelledCoeff(1,1) = coeff(1,5);
-    for i = 2:1907
-        modelledCoeff(i,1) = ARCoeff5.Constant + cell2mat(ARCoeff5.AR) .* modelledCoeff(i-1,1);
-    end
-    w(:,5) = coeff(:,5) - modelledCoeff(:,1);
-
-clear i;
-
-
-% %% Ermitteln der Kovarianzen: cov(w) = R, cov(coeff) = Q;
-% R = cov(w);
-% Q = cov(coeff);
-
-%% Ermitteln der Kovarianzen: cov(w) = R, cov(coeff) = Q;
-R = cov(v);
-Q = cov(w);
-u = eye(5);
-% B = Koeffizient 1 den wir finden wollen: A
-% F = Koeffizient 2 den wir finden wollen: B
-X = coeff;
-P = cov(coeff);
-%% 
 
---
+++ kalmanPred.m
@@ -0,0 +1,36 @@
+function [ mu ] = kalmanPred( coeffKalmanOut, epsOut, coeffOut, uniqueDates, filteredDataCall, model, stopOut )
+%kalmanPred predicts the coefficients for the linear model to evaluate the
+%volatility one day ahead. This function is used for testing the Kalman
+%filter out-of-sample.
+
+%find parameter for VAR(1) model
+Spec = vgxset('n',6,'nAR',1, 'Constant',true);
+EstSpec1 = vgxvarx(Spec,coeffOut);
+estParam1 = evalCoeffVar(coeffOut,EstSpec1.a,EstSpec1.AR);
+
+volaOut = evalVola(filteredDataCall(1:stopOut,:),coeffOut,model);
+
+residualCoeff = coeffOut - estParam1;
+residualVola = filteredDataCall.implVol(1:stopOut,:) - volaOut;
+
+A = EstSpec1.AR{1,1};
+omega = cov(residualCoeff);
+
+    %time update
+    mu = A * coeffKalmanOut(end,:)' + EstSpec1.a;
+    epsOut = A * epsOut * A' + omega;
+    %measurement update
+    thisDate = uniqueDates(size(coeffOut,1)+1);
+    [thisObs,thisObsSize] = getObs(thisDate,filteredDataCall);
+    H = [ones(thisObsSize,1), thisObs.Moneyness, thisObs.Moneyness.^2, thisObs.TimeToMaturity, thisObs.TimeToMaturity.^2, thisObs.Moneyness .* thisObs.TimeToMaturity];
+    v = thisObs.implVol - H*mu;
+    R = cov(residualVola)*eye(thisObsSize);
+    tau = H*epsOut*H' + R;    
+    %Kalman gain:
+    K = epsOut * H'*tau^-1;
+    
+    mu = mu + K*v;
+    mu = mu';
+
+end
+
